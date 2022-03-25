1ceb570f2232c2a9fe3075969a2c8701e72cf95e
Bepo0328 <82445568+Bepo0328@users.noreply.github.com>
2022-03-25 22:25:24 +0900

    NetflixCloneApp: Create Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_clone_app/screens/screens.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,12 +15,9 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.black,
       ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Main Screen'),
-        ),
-      ),
+      home: const HomeScreen(),
     );
   }
 }
